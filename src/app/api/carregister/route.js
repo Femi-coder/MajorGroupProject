2426bd8b164b955588bef59696dc1a883943d4df
Femi-coder <B00152778@mytudublin.ie>
2025-01-30 18:42:06 +0000

    aa

--- src/app/api/carregister/route.js
+++ src/app/api/carregister/route.js
@@ -21,7 +21,7 @@ export async function POST(req) {
         }
 
         // MongoDB connection
-        const uri = 'mongodb+srv://Femi:password_123@ecowheelsdublin.zpsyu.mongodb.net/?retryWrites=true&w=majority&appName=EcoWheelsDublin';
+        const uri = 'mongodb+srv://Femi:password_123@ecowheelsdublin.zpsyu.mongodb.net';
         const client = new MongoClient(uri);
         const dbName = 'carrental'; // database name
 
