11728e4f4cfd427e645547c894ac585de7b8a2a3
Femi-coder <B00152778@mytudublin.ie>
2025-02-10 20:26:51 +0000

    c

--- src/app/majorgroupproject/page.js
+++ src/app/majorgroupproject/page.js
@@ -283,6 +283,7 @@ export default function MyApp() {
                                 }
                             }}
                         >
+                            Explore Rental Options
                         </Button>
                     </Box>
                 </Box>
