e9c581c292aefe7a67005785746b676492986051
AlinFlor <145985974+AlinFlor@users.noreply.github.com>
2025-02-16 17:46:35 +0000

    Update VehicleList.js

--- src/app/api/VehicleList/VehicleList.js
+++ src/app/api/VehicleList/VehicleList.js
@@ -14,10 +14,10 @@ const VehicleList = () => {
     const fetchVehicles = async () => {
       const data = [
         { id: 1, make: "BYD", model: "Dolphin", year: 2022, price: 50, image: "https://www.byd.com/content/dam/byd-site/eu/electric-cars/dolphin/xl/Dolphin-exterior-04-SkiingwhiteUrbangrey-xl.jpg" },
-        { id: 2, make: "Tesla", model: "Model S", year: 2020, price: 45, image: "https://static1.topspeedimages.com/wordpress/wp-content/uploads/2023/06/tesla-model-s-plaid-2.jpg" },
-        { id: 3, make: "Tesla", model: "Model 3", year: 2021, price: 40, image: "https://www.tesla.com/ownersmanual/images/GUID-B5641257-9E85-404B-9667-4DA5FDF6D2E7-online-en-US.png" },
-        { id: 4, make: "Tesla", model: "Model X", year: 2022, price: 70, image: "https://images.prismic.io/carwow/c340a77d-af56-4562-abfb-bd5518ccb292_2023+Tesla+Model+X+front+quarter+moving.jpg" },
-        { id: 5, make: "Tesla", model: "Model Y", year: 2023, price: 80, image: "https://images.hgmsites.net/lrg/2022-tesla-model-y-performance-awd-angular-front-exterior-view_100833533_l.jpg" },
+        { id: 2, make: "Tesla", model: "Model S", year: 2023, price: 45, image: "https://static1.topspeedimages.com/wordpress/wp-content/uploads/2023/06/tesla-model-s-plaid-2.jpg" },
+        { id: 3, make: "Tesla", model: "Model 3", year: 2022, price: 40, image: "https://www.tesla.com/ownersmanual/images/GUID-B5641257-9E85-404B-9667-4DA5FDF6D2E7-online-en-US.png" },
+        { id: 4, make: "Tesla", model: "Model X", year: 2021, price: 70, image: "https://images.prismic.io/carwow/c340a77d-af56-4562-abfb-bd5518ccb292_2023+Tesla+Model+X+front+quarter+moving.jpg" },
+        { id: 5, make: "Tesla", model: "Model Y", year: 2020, price: 80, image: "https://images.hgmsites.net/lrg/2022-tesla-model-y-performance-awd-angular-front-exterior-view_100833533_l.jpg" },
       ];
       setVehicles(data);
       setFilteredVehicles(data);
